cf8d96a9e760e34d408f676057a3a82aaf8b9de4
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-01-11 22:50:13 +0100

    Simplify signal typing

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -20,7 +20,7 @@ export class IndexComponent {
   marginHeight = 16;
 
   canvas = viewChild.required<ElementRef<HTMLCanvasElement>>('canvas');
-  scene = viewChild(SceneDirective);
+  scene = viewChild.required(SceneDirective);
 
   props = {
     renderPointStars: true,
@@ -33,7 +33,7 @@ export class IndexComponent {
 
   ngOnInit(): void {
     this.reflow(window);
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
     // and then render the gui element
   }
 
@@ -46,7 +46,7 @@ export class IndexComponent {
       canvas.height = height;
     }
     this.reflow(window);
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   private reflow(w: Window): void {
@@ -62,7 +62,7 @@ export class IndexComponent {
 
   onFinishChangeSeed(seed: string|number|undefined): void {
     this.props.seed = seed;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   onFinishChangeWidth(width: number): void {
@@ -79,26 +79,26 @@ export class IndexComponent {
 
   onChangeRenderPointStars(renderPointStars: boolean): void {
     this.props.renderPointStars = renderPointStars;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   onChangeRenderStars(renderStars: boolean): void {
     this.props.renderStars = renderStars;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   onChangeRenderSun(renderSun: boolean): void {
     this.props.renderSun = renderSun;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   onChangeRenderNebulae(renderNebulae: boolean): void {
     this.props.renderNebulae = renderNebulae;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 
   onChangeShortScale(shortScale: boolean): void {
     this.props.shortScale = shortScale;
-    this.scene()!.render(this.props);
+    this.scene().render(this.props);
   }
 }
