1c968a5aa8752951b4ae43acb26c4e135f07614d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-01-12 01:23:54 +0100

    Finish GUI

--- src/app/index/gui/gui.component.ts
+++ src/app/index/gui/gui.component.ts
@@ -1,4 +1,5 @@
 import { Component, input, output } from '@angular/core';
+import { generateRandomSeed } from '../../../util/random';
 
 @Component({
   selector: 'app-gui',
@@ -26,7 +27,41 @@ export class GuiComponent {
   readonly changeRenderNebulae = output<boolean>();
   readonly changeShortScale = output<boolean>();
 
+  visible = true;
+
   onSeedChange(event: Event): void {
     this.finishChangeSeed.emit((event.target as HTMLInputElement).value);
+  }
+
+  onClickRandomizeSeed(): void {
+    this.finishChangeSeed.emit(generateRandomSeed());
+  }
+
+  onWidthChange(event: Event): void {
+    this.finishChangeWidth.emit(parseInt((event.target as HTMLInputElement).value));
+  }
+
+  onHeightChange(event: Event): void {
+    this.finishChangeHeight.emit(parseInt((event.target as HTMLInputElement).value));
+  }
+
+  onRenderPointStarsChange(event: Event): void {
+    this.changeRenderPointStars.emit((event.target as HTMLInputElement).checked);
+  }
+
+  onRenderStarsChange(event: Event): void {
+    this.changeRenderStars.emit((event.target as HTMLInputElement).checked);
+  }
+
+  onRenderSunChange(event: Event): void {
+    this.changeRenderSun.emit((event.target as HTMLInputElement).checked);
+  }
+
+  onRenderNebulaeChange(event: Event): void {
+    this.changeRenderNebulae.emit((event.target as HTMLInputElement).checked);
+  }
+
+  onShortScaleChange(event: Event): void {
+    this.changeShortScale.emit((event.target as HTMLInputElement).checked);
   }
 }
